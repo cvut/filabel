e5186662fbd241bb0770c4500709085c3d9d00cd
Miro Hrončok <miro@hroncok.cz>
2018-10-03 10:35:05 +0200

    Allow using required options

--- test/test_errors.py
+++ test/test_errors.py
@@ -3,40 +3,47 @@ from helper import run, config
 
 def test_no_config():
     cp = run('')
-    assert cp.returncode == 1
+    assert cp.returncode != 0
     assert not cp.stdout
     assert (
         cp.stderr == 'Auth configuration not supplied!\n' or
-        cp.stderr == 'Labels configuration not supplied!\n'
+        cp.stderr == 'Labels configuration not supplied!\n' or
+        'Missing option' in cp.stderr
     )
 
 
 def test_no_auth_config():
     cp = run(f'--config-labels "{config("labels.empty.cfg")}"')
-    assert cp.returncode == 1
+    assert cp.returncode != 0
     assert not cp.stdout
-    assert cp.stderr == 'Auth configuration not supplied!\n'
+    assert (
+        cp.stderr == 'Auth configuration not supplied!\n' or
+        'Missing option' in cp.stderr
+    )
 
 
 def test_unusable_auth_config():
     cp = run(f'--config-auth "{config("empty_file.cfg")}" '
              f'--config-labels "{config("labels.empty.cfg")}"')
-    assert cp.returncode == 1
+    assert cp.returncode != 0
     assert not cp.stdout
     assert cp.stderr == 'Auth configuration not usable!\n'
 
 
 def test_no_labels_config():
     cp = run(f'--config-auth "{config("auth.fff.cfg")}"')
-    assert cp.returncode == 1
+    assert cp.returncode != 0
     assert not cp.stdout
-    assert cp.stderr == 'Labels configuration not supplied!\n'
+    assert (
+        cp.stderr == 'Labels configuration not supplied!\n' or
+        'Missing option' in cp.stderr
+    )
 
 
 def test_unusable_labels_config():
     cp = run(f'--config-labels "{config("empty_file.cfg")}" '
              f'--config-auth "{config("auth.fff.cfg")}"')
-    assert cp.returncode == 1
+    assert cp.returncode != 0
     assert not cp.stdout
     assert cp.stderr == 'Labels configuration not usable!\n'
 
@@ -45,7 +52,7 @@ def test_invalid_repolsug():
     cp = run(f'--config-labels "{config("labels.empty.cfg")}" '
              f'--config-auth "{config("auth.fff.cfg")}" '
              'foobar')
-    assert cp.returncode == 1
+    assert cp.returncode != 0
     assert not cp.stdout
     assert cp.stderr == 'Reposlug foobar not valid!\n'
 
@@ -54,6 +61,6 @@ def test_invalid_second_repolsug():
     cp = run(f'--config-labels "{config("labels.empty.cfg")}" '
              f'--config-auth "{config("auth.fff.cfg")}" '
              'xyz/abc foobar')
-    assert cp.returncode == 1
+    assert cp.returncode != 0
     assert not cp.stdout
     assert cp.stderr == 'Reposlug foobar not valid!\n'
