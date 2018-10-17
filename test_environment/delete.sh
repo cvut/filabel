a390024243a8f7fcfe6daae6c37c1fb8fcfa6963
Ondrysak <nankaond@fit.cvut.cz>
2018-10-17 16:15:45 +0200

    Delete filabel-testrepo-everybody
    crashes setup.sh if exists

--- test_environment/delete.sh
+++ test_environment/delete.sh
@@ -4,3 +4,5 @@ for I in {1..4}; do
   echo "HTTP DELETE https://api.github.com/repos/${GH_USER}/filabel-testrepo${I}"
   curl --header "Authorization: token ${GH_TOKEN}" -X DELETE https://api.github.com/repos/${GH_USER}/filabel-testrepo${I}
 done
+echo "HTTP DELETE https://api.github.com/repos/${GH_USER}/filabel-testrepo-everybody"
+curl --header "Authorization: token ${GH_TOKEN}" -X DELETE https://api.github.com/repos/${GH_USER}/filabel-testrepo-everybody
